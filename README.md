1e85c76480d789574adb08ccc65dbfcf896c321f
PRAMOD A JINGADE <avj2352@gmail.com>
2020-04-13 23:22:22 +0800

    added configuration

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # OAuth using Nest JS and React
 
+![Logo](./design/logo.png)
+
 This is a simple POC which shows how to integrate social logins (for eg: Google) with Nest JS and React (Typescript)
 
 ## Important Links
 
 - [Nest Authentication using OAuth 2.0](https://github.com/workfel/nest-auth)
 - [Medium article - Nest JS using OAuth 2.0](https://medium.com/digikare/google-sign-in-authentication-with-nestjs-a6a4bb7de51)
+- [Heroku Link](https://oauth-nest.herokuapp.com/)
+
+## Environment Variables
+
+- The following `environment` variables need to be set (Heroku / AWS)
+- Refer the YAML file in `config > default.yml`
+
+```javascript
+  const secret = process.env.secret ? process.env.secret : config.get('jwt').secret;
+  const PORT = process.env.port ? process.env.port : config.get('server').port;
+```
+
+## Google Developer Setting Instructions
 
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WsAdapter } from '@nestjs/websockets';
+import * as socketio from 'socket.io';
+import * as expressSession from 'express-session';
+import * as passport from 'passport';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const secret = process.env.secret ? process.env.secret : config.get('jwt').secret;
+  const PORT = process.env.port ? process.env.port : config.get('server').port;
+  // Add websocket on your server
+  app.useWebSocketAdapter(new WsAdapter(app.getHttpServer()));
+  // Connecting sockets to the server and adding them to the request
+  // so that we can access them later in the controller
+  const io = socketio(app.getHttpServer());
+  app.setGlobalPrefix(io);
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(expressSession({
+    secret: secret,
+    resave: true,
+    saveUninitialized: true,
+  }));
+
+  const server = await app.listen(PORT);
+  console.log(`Server listening on PORT - ${server.address().port}`);
+
 }
 bootstrap();
